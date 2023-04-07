0fd1df98b560e198051e5a23147f46b9d43c8faa
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-04-07 18:54:19 +0700

    feat: add a new step that runs e2e tests to the workflow

--- cypress/e2e/phonebook.spec.cy.js
+++ cypress/e2e/phonebook.spec.cy.js
@@ -1,6 +1,6 @@
 describe('phonebook app', () => {
   it('displays data from server', () => {
-    cy.visit('http://localhost:3001')
+    cy.visit('https://damp-morning-4577.fly.dev/')
     cy.contains('Michael Nguyen').should('be.visible')
     // cy.get('.contact-info').should('be.visible')
   })
