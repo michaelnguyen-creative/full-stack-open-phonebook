0b7d588be6763096441eb11c99e73ca40a726523
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-04-08 11:11:22 +0700

    experiment with running e2e test config on ci

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 import app from './server/app.js'
-import config from './config/common.js'
+import config from './server/utils/config.js'
 
 const startServer = async () => {
   console.log('connecting to MongoDB at', config.MONGODB_URI)
--- server/utils/config.js
+++ server/utils/config.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV !== 'production') {
   dotenv.config()
 }
 
-const { PORT } = process.env
+const PORT = process.env.PORT || 3001
 const { MONGODB_URI } = process.env
 
 export default {
