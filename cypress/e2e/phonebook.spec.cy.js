c7c61f9d313338bb5c694b875ce63042b5d0ebea
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-05-19 17:28:01 +0700

    change cypress test to visit localhost:8080

--- cypress/e2e/phonebook.spec.cy.js
+++ cypress/e2e/phonebook.spec.cy.js
@@ -1,6 +1,6 @@
 describe('phonebook app', () => {
   it('displays data from server', () => {
-    cy.visit('http://localhost:3001')
+    cy.visit('http://localhost:8080')
     cy.contains('Michael Nguyen').should('be.visible')
     // cy.get('.contact-info').should('be.visible')
   })
